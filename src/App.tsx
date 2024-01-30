867c9c519d78054473f036e1b09a6cb78f9a48ec
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2024-01-30 16:57:47 -0700

    Create app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { DirectToBoot } from './DirectToBoot';
+
+function App() {
+  return (
+    <div>
+      <DirectToBoot orderId="order-id" />
+    </div>
+  );
+}
+
+export default App;
