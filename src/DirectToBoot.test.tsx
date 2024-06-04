f12bf9213c1315c9e3fecf635386c1371edb0b24
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2024-06-04 17:34:07 -0600

    Use axios and miragejs

--- src/DirectToBoot.test.tsx
+++ src/DirectToBoot.test.tsx
@@ -1,16 +1,50 @@
-import {render, screen } from '@testing-library/react';
-import React from 'react';
-import {DirectToBoot} from './DirectToBoot';
+import { render, screen, waitFor } from "@testing-library/react";
+import React from "react";
+import { DirectToBoot } from "./DirectToBoot";
+import { createServer, Server } from "miragejs";
 
-describe('Direct To Boot', () => {
-    it('renders a section for direct to boot', () => {
-        render(<DirectToBoot orderId="order-id" />);
-        expect(screen.getByText('Direct To Boot')).toBeInTheDocument();
-        expect(screen.getByText('We are preparing your order...')).toBeInTheDocument();
-        
-        const button = screen.getByRole('button');
-        expect(button).toBeInTheDocument();
-        expect(button).toBeDisabled();
+let server: Server;
+
+describe("Direct To Boot", () => {
+  beforeEach(() => {
+    server = createServer({});
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
+  it("renders a section for direct to boot", () => {
+    render(<DirectToBoot orderId="order-id" />);
+    expect(screen.getByText("Direct To Boot")).toBeInTheDocument();
+    expect(
+      screen.getByText("We are preparing your order...")
+    ).toBeInTheDocument();
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toBeDisabled();
+  });
+
+  it("enables button when the order is ready", async () => {
+    server.get("/api/order/:id", (schema, request) => {
+      return {
+        id: request.params.id,
+        status: "ready",
+      };
     });
-    }
-);
+
+    render(<DirectToBoot orderId="order-id" />);
+    expect(screen.getByText("Direct To Boot")).toBeInTheDocument();
+    expect(
+      screen.getByText("We are preparing your order...")
+    ).toBeInTheDocument();
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toBeDisabled();
+
+    // wait for
+    await waitFor(() => expect(button).toBeEnabled(), { timeout: 3000 });
+  });
+});
--- src/DirectToBoot.tsx
+++ src/DirectToBoot.tsx
@@ -1,21 +1,28 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 interface DirectToBootProps {
-    orderId: string;
-    }
+  orderId: string;
+}
 
 function DirectToBoot({ orderId }: DirectToBootProps) {
+  const [status, setStatus] = useState<string>("initialised");
+
+  useEffect(() => {
+    axios.get(`/api/orders/${orderId}`).then((response) => {
+      if (response.data.status === "ready") {
+        setStatus("ready");
+      }
+    });
+  }, [orderId]);
+
   return (
-    <div >
-        <h1>Direct To Boot</h1>
-        <p>We are preparing your order...</p>
-        <button
-            onClick={() => {
-                console.log('I am here');
-            }}
-            disabled={true}
-        >I'm here</button>  
+    <div>
+      <h1>Direct To Boot</h1>
+      <p>We are preparing your order...</p>
+      <button disabled={status !== "ready"}>I'm here</button>
     </div>
   );
 }
 
-export { DirectToBoot};
+export { DirectToBoot };
