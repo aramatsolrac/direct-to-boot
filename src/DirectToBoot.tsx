c617e87fdb14046c292ceaa2c2c74ad2f54bdeaa
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2024-10-28 17:48:10 -0600

    Update DirectToBoot.tsx

--- src/DirectToBoot.tsx
+++ src/DirectToBoot.tsx
@@ -5,22 +5,41 @@ interface DirectToBootProps {
   orderId: string;
 }
 
+function getMessage(status: string) {
+  switch (status) {
+    case "initialised":
+      return "We are preparing your order..";
+    case "ready":
+      return "Please click the button when you have arrived. One of our friendly staff will bring your order to you";
+    case "error":
+      return "Seems something went wrong, you can the following number to notify us instead.";
+  }
+}
+
 function DirectToBoot({ orderId }: DirectToBootProps) {
   const [status, setStatus] = useState<string>("initialised");
 
   useEffect(() => {
-    axios.get(`/api/orders/${orderId}`).then((response) => {
-      if (response.data.status === "ready") {
-        setStatus("ready");
-      }
-    });
+    axios
+      .get(`/api/orders/${orderId}`)
+      .then((response) => {
+        if (response.data.status === "ready") {
+          setStatus("ready");
+        }
+      })
+      .catch((e) => {
+        setStatus("error");
+      });
   }, [orderId]);
 
   return (
     <div>
       <h1>Direct To Boot</h1>
       <p>We are preparing your order...</p>
-      <button disabled={status !== "ready"}>I'm here</button>
+      {status === "error" && <a href="tel: 13343434">04 23 33</a>}
+      {status !== "error" && (
+        <button disabled={status !== "ready"}>I'm here</button>
+      )}
     </div>
   );
 }
